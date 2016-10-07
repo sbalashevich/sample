7f757a384d30ca303abb0d27d54a2efd001af557
Siarhei Balashevich <balashevich@gmail.com>
2016-10-07 16:18:53 +0300

    Sample spring boot application

---
+++ src/main/java/com/altoros/sample/SampleApplication.java
@@ -0,0 +1,20 @@
+package com.altoros.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SampleApplication {
+
+	@RequestMapping("/version")
+	public String getVersion(){
+		return "v2.1";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleApplication.class, args);
+	}
+}
